ad45e473c945ffe1360d74a355fe18b07e1b0e8a
LeoMosiah <leonardomosiah@gmail.com>
2019-01-14 15:24:36 -0200

    adjusting modal

--- src/presentation/components/NavBar.js
+++ src/presentation/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core";
 import HomeOutlined from "@material-ui/icons/HomeOutlined";
@@ -10,34 +10,50 @@ import AppBar from "@material-ui/core/AppBar";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import { styles } from "./styles/NavBar";
 import Typography from "@material-ui/core/es/Typography/Typography";
-import { Modal } from './Modal';
+import { ModalComponent } from './Modal';
 
-function NavBarComponent({ classes }) {
-  return (
-    <AppBar position="static" color="inherit">
-      <Toolbar className={classes.navBarContainer}>
-        <div className={classes.iconContainer}>
-          <HomeOutlined />
-          <Typography className={classes.iconName}>Home</Typography>
-        </div>
-        <div className={classes.iconContainer}>
-          <FlashOnOutlined />
-          <Typography className={classes.iconName}>Moments</Typography>
-        </div>
-        <div className={classes.iconContainer}>
-          <NotificationsOutlined />
-          <Typography className={classes.iconName}>Notifications</Typography>
-        </div>
-        <div className={classes.iconContainer}>
-          <EmailOutlined />
-          <Typography className={classes.iconName}>Messages</Typography>
-        </div>
-        <AccountCircle className={classes.accountIcon} />
-        <button className={classes.button}>Twettar</button>
-      </Toolbar>
-      <Modal Trigger={<button className={classes.button}>Twettar</button>}/>
-    </AppBar>
-  );
+class NavBarComponent extends Component {
+  state = {
+    isModalOpen : false
+  };
+  handleOpenModal = () => {
+    this.setState({ isModalOpen: true });
+  };
+  handleCloseModal = () => {
+    this.setState({ isModalOpen: false });
+  };
+  render() {
+    const { classes } = this.props;
+    const { isModalOpen } = this.state;
+    return (
+      <AppBar position="static" color="inherit">
+        <Toolbar className={classes.navBarContainer}>
+          <div className={classes.iconContainer}>
+            <HomeOutlined/>
+            <Typography className={classes.iconName}>Home</Typography>
+          </div>
+          <div className={classes.iconContainer}>
+            <FlashOnOutlined/>
+            <Typography className={classes.iconName}>Moments</Typography>
+          </div>
+          <div className={classes.iconContainer}>
+            <NotificationsOutlined/>
+            <Typography className={classes.iconName}>Notifications</Typography>
+          </div>
+          <div className={classes.iconContainer}>
+            <EmailOutlined/>
+            <Typography className={classes.iconName}>Messages</Typography>
+          </div>
+          <AccountCircle className={classes.accountIcon}/>
+          <button className={classes.button} onClick={() => this.handleOpenModal()}>Twettar</button>
+        </Toolbar>
+        <ModalComponent
+          isModalOpen={isModalOpen}
+          handleClose={this.handleCloseModal}
+        />
+      </AppBar>
+    );
+  }
 }
 
 export const NavBar = withStyles(styles)(NavBarComponent);
