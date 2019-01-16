b6244e7f06733475038b313244d864a956b872a2
LeoMosiah <leonardomosiah@gmail.com>
2019-01-16 20:30:10 -0200

    main

--- src/presentation/components/styles/Main.js
+++ src/presentation/components/styles/Main.js
@@ -1,9 +1,6 @@
 export const styles = {
   container: {
     height: "100vh",
-    backgroundColor: "#e6ecf0",
-    padding: "10px",
-    paddingLeft: "20px",
-    paddingRight: "20px"
+    backgroundColor: "#e6ecf0"
   }
 };
