e8ce978ca317983c7b807f0dc0733c3ca8b155d9
LeoMosiah <leonardomosiah@gmail.com>
2019-01-16 12:53:14 -0200

    hosting fixing

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,7 @@
 const functions = require("firebase-functions");
-const app = require('./src/app');
 
 module.exports = {
   helloWorld: functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
   }),
-  app: functions.https.onRequest(app)
 };
